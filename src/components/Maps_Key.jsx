e8c9a2a6e92c5901a27ade48b9f6a8b133f38388
jackmadethat <jbeven@ekium.com.au>
2025-10-16 09:49:50 +1030

    Fixed URL, key image

--- src/components/Maps_Key.jsx
+++ src/components/Maps_Key.jsx
@@ -12,7 +12,7 @@ const MapsKey = () => {
       <div className="textCenter">
         <button className="disclaimerBtn" onClick={toggleKey} style={{ marginBottom: 20 + 'px' }}><span style={{ color: 'white' }}>SHOW KEY</span></button>
       </div>
-      {keyVisible && <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/MapKey.JPG" className="imgGraphic" style={{ maxWidth: 400 + 'px', paddingBottom: 20 + 'px' }} />}
+      {keyVisible && <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/MapRule.JPG" className="imgGraphic" style={{ maxWidth: 400 + 'px', paddingBottom: 20 + 'px' }} />}
     </>
   )
 }
