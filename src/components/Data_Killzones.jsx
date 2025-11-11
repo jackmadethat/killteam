4c7e3ac2845898ad1d875f9713ce667dcfe09083
jackmadethat <jbeven@ekium.com.au>
2025-11-12 09:36:53 +1030

    Updated killzone data with generic types

--- src/components/Data_Killzones.jsx
+++ src/components/Data_Killzones.jsx
@@ -1,182 +1,182 @@
 export const Killzones = [
   {
     id: 1,
-    killzone: 'Tomb World',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-tw-1-min.png" className="mapImage" />
   },
   {
     id: 2,
-    killzone: 'Tomb World',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-tw-2-min.png" className="mapImage" />
   },
   {
     id: 3,
-    killzone: 'Tomb World',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-tw-3-min.png" className="mapImage" />
   },
   {
     id: 4,
-    killzone: 'Tomb World',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-tw-4-min.png" className="mapImage" />
   },
   {
     id: 5,
-    killzone: 'Tomb World',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-tw-5-min.png" className="mapImage" />
   },
   {
     id: 6,
-    killzone: 'Tomb World',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-tw-6-min.png" className="mapImage" />
   },
   {
     id: 7,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-1-min.png" className="mapImage" />
   },
   {
     id: 8,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-2-min.png" className="mapImage" />
   },
   {
     id: 9,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-3-min.png" className="mapImage" />
   },
   {
     id: 10,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-4-min.png" className="mapImage" />
   },
   {
     id: 11,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-5-min.png" className="mapImage" />
   },
   {
     id: 12,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-6-min.png" className="mapImage" />
   },
   {
     id: 13,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-7-min.png" className="mapImage" />
   },
   {
     id: 14,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-8-min.png" className="mapImage" />
   },
   {
     id: 15,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-9-min.png" className="mapImage" />
   },
   {
     id: 16,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-10-min.png" className="mapImage" />
   },
   {
     id: 17,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-11-min.png" className="mapImage" />
   },
   {
     id: 18,
-    killzone: 'Volkus',
+    killzone: 'Ruins',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-vk-12-min.png" className="mapImage" />
   },
   {
     id: 19,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-1-min.png" className="mapImage" />
   },
   {
     id: 20,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-2-min.png" className="mapImage" />
   },
   {
     id: 21,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-3-min.png" className="mapImage" />
   },
   {
     id: 22,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-4-min.png" className="mapImage" />
   },
   {
     id: 23,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-5-min.png" className="mapImage" />
   },
   {
     id: 24,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-6-min.png" className="mapImage" />
   },
   {
     id: 25,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-7-min.png" className="mapImage" />
   },
   {
     id: 26,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-8-min.png" className="mapImage" />
   },
   {
     id: 27,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-9-min.png" className="mapImage" />
   },
   {
     id: 28,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-10-min.png" className="mapImage" />
   },
   {
     id: 29,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-11-min.png" className="mapImage" />
   },
   {
     id: 30,
-    killzone: 'Bheta-Decima',
+    killzone: 'Ocean Rig',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-bd-12-min.png" className="mapImage" />
   },
   {
     id: 31,
-    killzone: 'Gallowdark',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-itd-1-min.png" className="mapImage" />
   },
   {
     id: 32,
-    killzone: 'Gallowdark',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-itd-2-min.png" className="mapImage" />
   },
   {
     id: 33,
-    killzone: 'Gallowdark',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-itd-3-min.png" className="mapImage" />
   },
   {
     id: 34,
-    killzone: 'Gallowdark',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-itd-4-min.png" className="mapImage" />
   },
   {
     id: 35,
-    killzone: 'Gallowdark',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-itd-5-min.png" className="mapImage" />
   },
   {
     id: 36,
-    killzone: 'Gallowdark',
+    killzone: 'Dungeon',
     image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/map-itd-6-min.png" className="mapImage" />
   },
   {
@@ -211,62 +211,62 @@ export const Killzones = [
   },
   {
     id: 43,
-    killzone: 'Octarius',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Octarius_01.JPG" className="mapImage" />
+    killzone: 'Ruins',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Ruins_01.JPG" className="mapImage" />
   },
   {
     id: 44,
-    killzone: 'Octarius',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Octarius_02.JPG" className="mapImage" />
+    killzone: 'Ruins',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Ruins_02.JPG" className="mapImage" />
   },
   {
     id: 45,
-    killzone: 'Octarius',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Octarius_03.JPG" className="mapImage" />
+    killzone: 'Ruins',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Ruins_03.JPG" className="mapImage" />
   },
   {
     id: 46,
-    killzone: 'Octarius',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Octarius_04.JPG" className="mapImage" />
+    killzone: 'Ruins',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Ruins_04.JPG" className="mapImage" />
   },
   {
     id: 47,
-    killzone: 'Octarius',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Octarius_05.JPG" className="mapImage" />
+    killzone: 'Ruins',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Ruins_05.JPG" className="mapImage" />
   },
   {
     id: 48,
-    killzone: 'Octarius',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Octarius_06.JPG" className="mapImage" />
+    killzone: 'Ruins',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Ruins_06.JPG" className="mapImage" />
   },
   {
     id: 49,
-    killzone: 'Mechelen',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mechelen_01.JPG" className="mapImage" />
+    killzone: 'Mixed',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mixed_01.JPG" className="mapImage" />
   },
   {
     id: 50,
-    killzone: 'Mechelen',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mechelen_02.JPG" className="mapImage" />
+    killzone: 'Mixed',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mixed_02.JPG" className="mapImage" />
   },
   {
     id: 51,
-    killzone: 'Mechelen',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mechelen_03.JPG" className="mapImage" />
+    killzone: 'Mixed',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mixed_03.JPG" className="mapImage" />
   },
   {
     id: 52,
-    killzone: 'Mechelen',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mechelen_04.JPG" className="mapImage" />
+    killzone: 'Mixed',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mixed_04.JPG" className="mapImage" />
   },
   {
     id: 53,
-    killzone: 'Mechelen',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mechelen_05.JPG" className="mapImage" />
+    killzone: 'Mixed',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mixed_05.JPG" className="mapImage" />
   },
   {
     id: 54,
-    killzone: 'Mechelen',
-    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mechelen_06.JPG" className="mapImage" />
+    killzone: 'Mixed',
+    image: <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/Mixed_06.JPG" className="mapImage" />
   },
 ]
