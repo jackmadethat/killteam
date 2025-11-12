f22faeccf9e6b0f886ada54f6736c37ca7a39da8
jackmadethat <jbeven@ekium.com.au>
2025-11-12 15:02:36 +1030

    Updated map rule

--- src/components/Maps_Key.jsx
+++ src/components/Maps_Key.jsx
@@ -10,11 +10,11 @@ export const MapsKey = () => {
   return (
     <>
       <div className="textCenter">
-        <button className="disclaimerBtn" onClick={toggleKey} style={{ marginBottom: 20 + 'px' }}><span style={{ color: 'white' }}>SHOW KEY</span></button>
+        <button className="disclaimerBtn" onClick={toggleKey} style={{ marginBottom: 20 + 'px' }}><span style={{ color: 'white' }}>{keyVisible ? "HIDE KEY" : "SHOW KEY"}</span></button>
         {keyVisible && 
           <div className="mapGrid">
               <figure>
-                <img src="https://raw.githubusercontent.com/jackmadethat/killteam/refs/heads/main/src/img/maps/MapRule.JPG" className="imgGraphic" style={{ maxWidth: 350 + 'px' }} />
+                <img src="https://raw.githubusercontent.com/jackmadethat/killteam/e8d52443d51a4881e372065a015fd275f8069a35/src/img/maps/MapRule.svg" className="imgGraphic" style={{ maxWidth: 500 + 'px' }} />
                 <p>View the Terrain Rules section for killzone-specific rules.</p>
                 <br />
               </figure>
