0013a88856d786793e3b14990a1acf197f8b4a7f
jackmadethat <jbeven@ekium.com.au>
2025-11-18 12:48:30 +1030

    Minor update to language

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -2,12 +2,14 @@ const Introduction = () => {
   return (
     <div className="sectionContent" id="introduction">
       <p><em>Kill Team</em> is a tactical skirmish tabletop game set in a dark, sci-fi future. Two rival teams of highly skilled operatives battle and complete objectives to achieve victory. It's a game focusing on small-scale battles between individual characters rather than big armies. The tight scale, shorter game length and low number of units makes Kill Team one of the most accessible ways to get into tabletop wargaming hobby.</p>
-      <p>The game is originally designed for Warhammer 40k miniatures, but any miniatures are fine as long as they comply with Kill Team Rules (base sizes, for example).</p>
-      <p>This compendium deviates greatly from the official ruleset, using many homebrew rules, details and features. While it loosely resembles the 2024 Kill Team rules, it shouldn't be used as a substitute for the official rulebook.</p>
+      <p>The game is originally designed for Games Workshop miniatures, but any miniatures are fine as long as they comply with Kill Team rules (base sizes, for example).</p>
+      <p>This compendium deviates greatly from Games Workshop's ruleset, using many homebrew rules, details and features. While it loosely resembles the GW Kill Team 2024 rules, it shouldn't be used as a substitute for Games Workshop's version.</p>
       <p>While the game is primarily played with 2 players, Co-Op and single player missions are included, and a great way to learn the ropes before playing against other players.</p>
       <h3>What you'll need to play Kill Team:</h3>
       <ul>
-        <li>A team for each player (between 6 and 15 models, depending on your faction)</li>
+        <li>A team for each player (between 6 and 15 models, depending on your faction)
+          <p className="indentNote"><em><a href="https://www.myminifactory.com/users/Puppetswar_Miniatures">Puppetswar</a> and <a href="https://www.myminifactory.com/users/onepagerules">One Page Rules</a> offer very affordable miniatures that are suitable for Kill Team.</em></p>
+        </li>
         <li>A play area (referred to as the 'killzone') of 30" x 22" or 703mm x 606mm divided into a 7x6 grid, depending on terrain setup</li>
         <div className="mapGrid">
           <img src="https://github.com/jackmadethat/killteam/raw/refs/heads/main/src/img/maps/Killzone_Standard_Blank.svg" className="mapImage" alt="30 x 22 inch killzone" />
